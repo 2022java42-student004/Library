d4661f04f26fa578a648d02af6b567aa583fe974
WIN-PN8UJ7TR6E8\student <2022java42-student004@la-study.com>
2022-05-19 12:22:44 +0900

    ログインサーブレット作成

--- path/Code/src/main/java/servlet/TopLoginServlt.java
+++ path/Code/src/main/java/servlet/TopLoginServlt.java
@@ -10,30 +10,17 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/TopLoginServlt")
 public class TopLoginServlt extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public TopLoginServlt() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
+		String inputID = request.getParameter("id");
+		String inputPass = request.getParameter("pass");
 	}
 
 }
